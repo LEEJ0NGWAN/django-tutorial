e8c44462a5566602d8616450b61dcc5db2e413ca
LEEJ0NGWAN <lastffx@gmail.com>
2020-05-20 14:27:45 +0900

    index part creation

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+import datetime
 
 # Create your views here.
+def current_datetime(request):
+    now =  datetime.datetime.now()
+    html = "<html><body>It is %s now"%(now)
+    return HttpResponse(200,html)
+
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
